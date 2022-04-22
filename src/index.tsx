5b425d41ae0b509732f31faf685cf42e178eb8d6
Steve Boyer <stephen.a.boyer@gmail.com>
2022-04-22 13:59:40 -0500

    Import app-wide ethereum provider from @usedapp and wrap app with it

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { DAppProvider } from "@usedapp/core";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,11 +10,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* We wrap our entire app with DAppProvider to allow access to Eth accounts */}
+    <DAppProvider config={{}}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
